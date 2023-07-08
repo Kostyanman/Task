f3e14965d95275b8a58edd804222277d9b02c752
«WoodY» <Bacerikovk@gmail.com>
2023-07-08 10:15:47 +0300

    Вывод от -N до N

---
+++ task01/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите первое целое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе целое число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number2 == number1 * number1)
+
+{
+    Console.WriteLine($"Да, число {number2} является квадратом числа {number1}");
+}
+else
+{
+    Console.WriteLine($"Нет, число {number2} Не является квадратом числа {number1}");
+}
